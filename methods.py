cead5fade5e61d8bd581ee075773e9fa605b206a
Adrian <reinieradrianluna@gmail.com>
2023-11-01 14:43:03 +0800

    reportId being a key with random id

--- methods.py
+++ methods.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
@@ -7,23 +8,26 @@ def initialization():
     def createReportTable():
         # Create a table if it does not exist
         cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS report
-                        (ReportID INTEGER PRIMARY KEY AUTOINCREMENT, 
-                        title TEXT, 
-                        checklist TEXT, 
-                        image_path TEXT, 
-                        details TEXT, 
-                        urgency TEXT,
-                        status TEXT)
+                    CREATE TABLE IF NOT EXISTS report (
+                    ReportID INTEGER PRIMARY KEY,
+                    title TEXT,
+                    checklist TEXT,
+                    image_path TEXT,
+                    details TEXT,
+                    urgency TEXT,
+                    status TEXT)
                     ''')
 
     def createStatusUpdate():
         cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS statusUpdates
-                        (ReportID INTEGER PRIMARY KEY AUTOINCREMENT, 
-                        status TEXT 
-                        )
-                    ''')
+            CREATE TABLE IF NOT EXISTS statusUpdate (
+                ReportID INTEGER,
+                title TEXT,
+                status TEXT,
+                FOREIGN KEY (ReportID) REFERENCES report(ReportID)
+            )
+        ''')
+
     
     createReportTable()
     createStatusUpdate()
@@ -48,6 +52,7 @@ def userReport():
         3: report3,
     }
     
+    reportID = random.randint(10, 999)
     title = input("Title for your report:")
     choice = input("Choose from any of the following [1]Robbery [2]Fire [3]Accident:")
     checklist = pre_defined_reports[int(choice)]()
@@ -57,17 +62,29 @@ def userReport():
     status = "pending"
     
     # Insert user input into the database
-    cursor.execute("INSERT INTO report (title, checklist, image_path, details, urgency, status) VALUES (?, ?, ?, ?, ?, ?)",
-               (title, checklist, image_path, details, urgency, status))
+    cursor.execute("INSERT INTO report (reportID, title, checklist, image_path, details, urgency, status) VALUES (?, ?, ?, ?, ?, ?, ?)",
+               (reportID, title, checklist, image_path, details, urgency, status))
+    
+    cursor.execute("INSERT INTO statusUpdate (reportID, title, status) VALUES (?, ?, ?)",
+               (reportID, title, status))
 
     
+    def printReport():
+        cursor.execute("SELECT * FROM report")
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            print(row)
+            
+    def printStatusUpdate():
+        cursor.execute("SELECT * FROM statusUpdate")
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            print(row)
     
-    cursor.execute("SELECT * FROM report")
-    rows = cursor.fetchall()
-    
-    for row in rows:
-        print(row)
-    
+    printReport()
+    printStatusUpdate()
 
 
 
